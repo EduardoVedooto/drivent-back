1372bb8aa43bee1a14ad98a19da5e6c765a1bac7
Laura <laura.tracierra@gmail.com>
2021-09-05 19:52:04 -0300

    fix: enrollment bug

--- src/entities/Hotel.ts
+++ src/entities/Hotel.ts
@@ -26,13 +26,14 @@ export default class Hotel extends BaseEntity {
   rooms: Room[];
 
   static async getHotelsForUser(userId: number) {
+    const details = [
+      "Você precisa ter confirmado o pagamento antes de fazer a escolha de hospedagem",
+    ];
     const enrollment = await Enrollment.getByUserIdWithAddress(userId);
-    if (!enrollment) throw new NotFoundError();
+    if(!enrollment) throw new CannotPickHotelError(details);
     const booking = await Booking.getByEnrollmentId(enrollment.id);
+    if (!booking) throw new CannotPickHotelError(details);
     if (!booking?.isPaid) {
-      const details = [
-        "Você precisa ter confirmado o pagamento antes de fazer a escolha de hospedagem",
-      ];
       throw new CannotPickHotelError(details);
     }
     return this.find();

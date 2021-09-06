40c373ac795e29a3dbd1d367a6e93c595a0e0398
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-06 20:16:43 -0300

    test: get /ticket-options

--- src/controllers/client/ticket.ts
+++ src/controllers/client/ticket.ts
@@ -5,8 +5,7 @@ import * as service from "@/services/client/ticket";
 
 export async function getTicketOptions(req: Request, res: Response) {
   const tiketOptions = await service.getTicketOptions();
-  if(!tiketOptions) return res.sendStatus(httpStatus.NOT_FOUND);
 
-  res.status(httpStatus.CREATED).send(tiketOptions);
+  res.status(httpStatus.OK).send(tiketOptions);
 }
 

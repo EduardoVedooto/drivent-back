de5de7c61c34644f20ba59dfa20836167ff0465f
Vitor Lourenco <vitor.registros@lourencos.net>
2021-09-09 16:07:13 -0300

    feat: get activities with date by location

--- src/entities/ActivityLocation.ts
+++ src/entities/ActivityLocation.ts
@@ -1,4 +1,10 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import Activities from "./Activities";
 
 @Entity("activityLocation")
@@ -9,6 +15,23 @@ export default class ActivityLocation extends BaseEntity {
   @Column()
   name: string;
 
-  @OneToMany(() => Activities, activities => activities.activityLocation)
+  @OneToMany(() => Activities, (activities) => activities.activityLocation)
   activities: Activities[];
+
+  static async getActivitiesWithDateId(dateId: number) {
+    const locationsWithActivities = await this.createQueryBuilder(
+      "activityLocation"
+    )
+      .leftJoinAndSelect("activityLocation.activities", "activities")
+      .where("activities.dateId = :dateId", { dateId })
+      .orderBy({ "activities.startsAt": "ASC", "activityLocation.name": "ASC" })
+      .getMany();
+    locationsWithActivities.forEach((location) => {
+      location.activities.forEach((activity) => {
+        delete activity.dateId;
+        delete activity.activityLocationId;
+      });
+    });
+    return locationsWithActivities;
+  }
 }

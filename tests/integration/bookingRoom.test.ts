1123fb6381b40ee71dff8d5c23b253b4aadcd5dd
Vitor Lourenco <vitor.registros@lourencos.net>
2021-09-09 22:33:49 -0300

    fix: should not return status 409

--- tests/integration/bookingRoom.test.ts
+++ tests/integration/bookingRoom.test.ts
@@ -11,9 +11,7 @@ import {
   createBasicTicketOptions,
 } from "../utils/app";
 import { createUser } from "../factories/userFactory";
-import {
-  createBookingWithHotel,
-} from "../factories/bookingFactory";
+import { createBookingWithHotel } from "../factories/bookingFactory";
 import { createEnrollment } from "../factories/enrollmentFactory";
 import Session from "@/entities/Session";
 import { createToken } from "@/utils/app";
@@ -33,7 +31,7 @@ beforeEach(async () => {
   settings = await createBasicSettings();
   const hotels = (await createBasicHotels()) as Hotel[];
   const rooms = await createBasicRooms(hotels);
-  roomId = rooms[2].id
+  roomId = rooms[2].id;
   await createBasicHotelOptions();
   await createBasicTicketOptions();
 });
@@ -44,35 +42,34 @@ afterAll(async () => {
 });
 
 async function generateData() {
-    const user = await createUser();
-    const token = createToken(user.id);
-    await Session.createNew(user.id, token);
-    const enrollment = await createEnrollment(user.id);
-    await createBookingWithHotel(enrollment.id);
-    return token;
+  const user = await createUser();
+  const token = createToken(user.id);
+  await Session.createNew(user.id, token);
+  const enrollment = await createEnrollment(user.id);
+  await createBookingWithHotel(enrollment.id);
+  return token;
 }
 
 describe("GET /rooms/:hotelID", () => {
-    it("should return status 404 when room doesn't exits", async() => {
-        const token = await generateData();
-        const body = { roomId: roomId + 1 };
-        const response = await agent.post(`/booking-room`).set(createAuthHeader(token)).send(body);
-        expect(response.status).toEqual(404);
-    })
-    it("should return status 201 when room exits", async() => {
-        const token = await generateData();
-        const body = { roomId };
-        const response = await agent.post(`/booking-room`).set(createAuthHeader(token)).send(body);
-        expect(response.status).toEqual(201);
-        expect(response.body).toHaveProperty("rooms");
-        expect(response.body).toHaveProperty("imgUrl");
-        expect(response.body).toHaveProperty("name");
-    })
-    it("should return status 409 when user already has a room", async() => {
-        const token = await generateData();
-        const body = { roomId };
-        await agent.post(`/booking-room`).set(createAuthHeader(token)).send(body);
-        const response = await agent.post(`/booking-room`).set(createAuthHeader(token)).send(body);
-        expect(response.status).toEqual(409);
-    })
-})
+  it("should return status 404 when room doesn't exits", async () => {
+    const token = await generateData();
+    const body = { roomId: roomId + 1 };
+    const response = await agent
+      .post("/booking-room")
+      .set(createAuthHeader(token))
+      .send(body);
+    expect(response.status).toEqual(404);
+  });
+  it("should return status 201 when room exits", async () => {
+    const token = await generateData();
+    const body = { roomId };
+    const response = await agent
+      .post("/booking-room")
+      .set(createAuthHeader(token))
+      .send(body);
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("rooms");
+    expect(response.body).toHaveProperty("imgUrl");
+    expect(response.body).toHaveProperty("name");
+  });
+});

85327aa649a719fe8af766de7f8a26dc5a42cd43
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-14 16:10:37 -0300

    fix: unit test service activities

--- tests/units/services/activities.test.ts
+++ tests/units/services/activities.test.ts
@@ -1,9 +1,9 @@
-import service from "@/services/client/activities";
+import * as service from "@/services/client/activities";
 import Activities from "@/entities/Activities"
 
 describe("getAllActivities", () => {
     it("returns all activities from entity", async () => {
-        jest.spyOn(Activities, "getAll").mockImplementationOnce(async () => {});
+        jest.spyOn(Activities, "getAll").mockImplementationOnce(async () => {return {} as Activities[]});
 
         const result = () => service.getAllActivities();
 
